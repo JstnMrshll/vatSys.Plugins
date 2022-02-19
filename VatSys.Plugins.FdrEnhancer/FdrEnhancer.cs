9e991576d8169546341fcfa40bac0a05f9acfa94
JstnMrshll <jstnmrshll@outlook.com>
2022-02-19 23:32:32 +1100

    Extra labels TXT & RX labels Check correct RFL & CFL by heading

--- VatSys.Plugins.FdrEnhancer/FdrEnhancer.cs
+++ VatSys.Plugins.FdrEnhancer/FdrEnhancer.cs
@@ -12,6 +12,11 @@ namespace VatSys.Plugins
     [Export(typeof(IPlugin))]
     public class FdrEnhancer : IPlugin
     {
+        const string stdMsgPrefix = ".";
+        //const string airportMsgPrefix = "*";
+
+        const string RAPIDAPI_KEY = "";
+
         ConcurrentDictionary<string, string> airlineCallsigns = new ConcurrentDictionary<string, string>();
         ConcurrentDictionary<string, string> airportNames = new ConcurrentDictionary<string, string>();
 
@@ -20,7 +25,7 @@ namespace VatSys.Plugins
 
         public void OnFDRUpdate(FDP2.FDR updated)
         {
-            if (updated.State == FDP2.FDR.FDRStates.STATE_PREACTIVE && updated.RunwayString != null && updated.RunwayString.Length > 0)
+            if (updated.LocalOpData == "" || updated.LocalOpData.StartsWith("."))
             {
                 string[] routeTextSections = updated.RouteNoParse.Split(' ');
 
@@ -34,27 +39,58 @@ namespace VatSys.Plugins
                     waypoint = routeTextSections[1];
                 }
 
-                if (airportName.Length + waypoint.Length > 13) airportName = airportName.Substring(0, (13 - waypoint.Length));
-                if (waypoint != "") sep = "*";
-
-                updated.LocalOpData = airportName + sep + waypoint;
-
-                //if (updated.IsTrackedByMe)
-                //updated.Remarks += $"/FDRE/ AIRPORT/{airportName} AIRLINE/{airlineCallsign}";
-
-                //updated.LabelOpData
-            }
-            else if (updated.State == FDP2.FDR.FDRStates.STATE_PREACTIVE && (updated.RunwayString==null || updated.RunwayString==""))
-            {
                 string airlineCallsign = LookupAirlineCallsign(updated.Callsign);
-                updated.LocalOpData = airlineCallsign;
+
+                if (updated.State == FDP2.FDR.FDRStates.STATE_PREACTIVE && updated.RunwayString == "" && airlineCallsign != "")
+                {
+                    updated.LocalOpData = stdMsgPrefix + airlineCallsign;
+                }
+                else if (updated.State == FDP2.FDR.FDRStates.STATE_PREACTIVE && updated.RunwayString == "" && airlineCallsign == "")
+                {
+                    updated.LocalOpData = stdMsgPrefix + airportName;
+                }
+                else if (updated.State == FDP2.FDR.FDRStates.STATE_PREACTIVE && updated.RunwayString != "")
+                {
+                    if (airportName.Length + waypoint.Length > 12) airportName = airportName.Substring(0, (13 - waypoint.Length));
+                    if (waypoint != "") sep = "*";
+
+                    updated.LocalOpData = stdMsgPrefix + airportName + sep + waypoint;
+
+                    //if (updated.IsTrackedByMe && !updated.OtherInfoString.Contains("/FDRENH/"))
+                    //updated.Remarks += $"/FDRENH/ AIRPORT/{airportName} AIRLINE/{airlineCallsign}";
+
+                }
             }
 
-            //if (updated.IsTrackedByMe && updated.TextOnly && (updated.GlobalOpData == null || updated.GlobalOpData == "" || !updated.GlobalOpData.Contains("TXT")))
-            //{
-            //    updated.GlobalOpData += (updated.GlobalOpData.Length > 0 ? " " : "") + "TXT";
-            //}
+            // Modify Global tags only if it is managed by "Me"
+            if (updated.IsTrackedByMe)
+            {
+                if (updated.TextOnly && !updated.GlobalOpData.Contains("^TXT"))
+                {
+                    updated.GlobalOpData = "^TXT";
+                }
+                else if (updated.ReceiveOnly && !updated.GlobalOpData.Contains("^RX"))
+                {
+                    updated.GlobalOpData = "^RX";
+                }
 
+                Coordinate source = vatsys.Airspace2.GetAirport(updated.DepAirport).LatLong;
+                Coordinate destination = vatsys.Airspace2.GetAirport(updated.DesAirport).LatLong;
+
+                double bearingBetweenAirports = CalculateBearingFromRads(source.LatitudeRads, source.LongitudeRads, destination.LatitudeRads, destination.LongitudeRads);
+
+                bool needsEven = (bearingBetweenAirports >= 180 && bearingBetweenAirports < 360);
+                bool evenRFL = (updated.RFL % 20 == 0);
+                bool evenCFL = (updated.CFLUpper % 20 == 0);
+
+                //if (updated.LocalOpData == "")
+                //    updated.LocalOpData += bearingBetweenAirports.ToString("##0");
+
+                if (updated.RFL > 0 && needsEven && !evenRFL && !updated.GlobalOpData.Contains(@"@RFL"))
+                    updated.GlobalOpData += @"@RFL";
+                if (updated.CFLUpper > 0 && needsEven && !evenCFL && !updated.GlobalOpData.Contains(@"@CFL"))
+                    updated.GlobalOpData += @"@CFL";
+            }
         }
 
         public void OnRadarTrackUpdate(RDP.RadarTrack updated)
@@ -97,7 +133,7 @@ namespace VatSys.Plugins
                     var client = new RestClient("https://aviation-reference-data.p.rapidapi.com/airline/" + callsignTextOnly);
                     var request = new RestRequest(Method.GET);
                     request.AddHeader("x-rapidapi-host", "aviation-reference-data.p.rapidapi.com");
-                    request.AddHeader("x-rapidapi-key", "f15025cd2cmsh3af33e491c76174p1c5381jsn03476d3f5108");
+                    request.AddHeader("x-rapidapi-key", RAPIDAPI_KEY);
                     IRestResponse response = client.Execute(request);
 
                     if (response.StatusCode == System.Net.HttpStatusCode.OK)
@@ -129,7 +165,7 @@ namespace VatSys.Plugins
                 var client = new RestClient("https://aviation-reference-data.p.rapidapi.com/airports/" + airportCode);
                 var request = new RestRequest(Method.GET);
                 request.AddHeader("x-rapidapi-host", "aviation-reference-data.p.rapidapi.com");
-                request.AddHeader("x-rapidapi-key", "f15025cd2cmsh3af33e491c76174p1c5381jsn03476d3f5108");
+                request.AddHeader("x-rapidapi-key", RAPIDAPI_KEY);
                 IRestResponse response = client.Execute(request);
 
                 if (response.StatusCode == System.Net.HttpStatusCode.OK)
@@ -147,6 +183,22 @@ namespace VatSys.Plugins
             return airportName;
         }
 
+        double DEG_PER_RAD = (180.0 / Math.PI);
+        public double CalculateBearingFromRads(double lat1, double lon1, double lat2, double lon2)
+        {
+            var dLon = lon2 - lon1;
+            var y = Math.Sin(dLon) * Math.Cos(lat2);
+            var x = Math.Cos(lat1) * Math.Sin(lat2) - Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(dLon);
+            double brg = DEG_PER_RAD * Math.Atan2(y, x);
+
+            if (brg < 0) brg += 360;
+            return brg;
+        }
+
+        public static double DegreesToRadians(double angle)
+        {
+            return angle * Math.PI / 180.0d;
+        }
     }
 
 }
